4372620f0ea63343a27efe3e1647d0f20a05a31b
hridoye <hridoysagor171@gmail.comm>
2022-09-11 13:01:58 -0600

    make single trending product using DOM

--- trending.js
+++ trending.js
@@ -4,10 +4,37 @@
     const LoadDataFunction = async (url) => { try { let response = await fetch(url, { method: "get", headers: { "businessid": `${BUSINESS_ID}`, } }); response = await response.json(); if (response.Error) { return console.log(response.Error) }; return response; } catch (e) { return }; };
 
     const treandingProduct = await LoadDataFunction(`https://api.soppiya.com/v2.1/widget/home/trending`);
-    // let FeatureImage = `https://www.soppiya.com/media/images/${BUSINESS_ID}/item/${flashSinleProduct?.itemId}/${flashSinleProduct?.image}`;
 
     async function showTrendingProduct(treandingProduct) {
-        console.log("treandingProduct", treandingProduct);
+        for (let i = 0; i < treandingProduct.length; i++) {
+            const singleProduct = treandingProduct[i];
+            console.log("singleProduct", singleProduct);
+            let FeatureImage = `https://www.soppiya.com/media/images/${BUSINESS_ID}/item/${singleProduct?._id}/${singleProduct?.image}`;
+
+            const s0507_trending_items_single_product = elementMaker("div", ["s0507_trending_items_single_product"]);
+
+            const s0507_product_top_area = elementMaker("div", ["s0507_product_top_area"]);
+            s0507_trending_items_single_product.appendChild(s0507_product_top_area);
+            const s0507_product_bottom_area = elementMaker("div", ["s0507_product_bottom_area"]);
+            s0507_trending_items_single_product.appendChild(s0507_product_bottom_area);
+
+            const s0507_product_img_wrapper = elementMaker("div", ["s0507_product_img_wrapper"]);
+            s0507_product_top_area.appendChild(s0507_product_img_wrapper);
+            const productImage = elementMaker("img", ["product_img"]);
+            s0507_product_img_wrapper.appendChild(productImage);
+
+
+
+
+
+
+
+
+
+
+            document.querySelector(".s0507_trending_items_home_all_product_wrapper").appendChild(s0507_trending_items_single_product);
+
+        }
 
 
     };
